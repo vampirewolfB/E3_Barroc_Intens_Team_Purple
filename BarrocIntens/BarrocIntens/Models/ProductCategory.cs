d82a72294b4cf703ae1e52e659c6716905e783f8
BerendFhost <vwberend@gmail.com>
2024-10-31 15:37:49 +0100

    Database setup
    Added the erd to project to easily all connections.
    Created all the models needed for the database.
    Setup a appsettings.json to generalize certain settings.
    Setup all the factories and such to seed test in the database when needed.

---
+++ BarrocIntens/BarrocIntens/Models/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BarrocIntens.Models
+{
+    internal class ProductCategory
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "tinyint")]
+        [Required]
+        public bool IsEmployeeOnly { get; set; }
+    }
+}
