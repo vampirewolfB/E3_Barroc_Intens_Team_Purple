1e629e321a75bdcf02643350c87fc428acf17b33
sheepy-v2 <117262261+sheepy-v2@users.noreply.github.com>
2024-11-06 07:31:40 +0100

    maintenance dashboard
    ive made the maintenance dashboard

--- BarrocIntens/BarrocIntens/App.xaml.cs
+++ BarrocIntens/BarrocIntens/App.xaml.cs
@@ -41,7 +41,7 @@ namespace BarrocIntens
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
+            m_window = new MaintenanceWindow();
             m_window.Activate();
         }
 
