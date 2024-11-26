7394437a8bb21be14216e785efdfffbe60806dd0
sheepy-v2 <117262261+sheepy-v2@users.noreply.github.com>
2024-11-26 13:56:44 +0100

    models for database made

---
+++ BarrocIntens/BarrocIntens/Models/ExpenseProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarrocIntens.Models
+{
+    internal class ExpenseProduct
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int ExpenseId { get; set; }
+        public Expense Expense { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ BarrocIntens/BarrocIntens/Models/QuoteProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarrocIntens.Models
+{
+    internal class QuoteProduct
+    {
+        public int Id { get; set; }
+        public int QuoteId { get; set; }
+        public Quote Quote { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
