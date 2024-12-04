cc1efbe8c39527377ff4cfd76beeef6318564d70
vampirewolfB <vwberend@gmail.com>
2024-12-04 14:01:05 +0100

    Customer create
    Added the customer create page.
    Made the email sender and added all the required things for it.

--- BarrocIntens/BarrocIntens/Sales/CustomerCreatePage.xaml.cs
+++ BarrocIntens/BarrocIntens/Sales/CustomerCreatePage.xaml.cs
@@ -12,6 +12,11 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System.Diagnostics;
+using BarrocIntens.Utility;
+using BarrocIntens.Utility.Database;
+using BCrypt.Net;
+using BarrocIntens.Utility.Emails;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +31,92 @@ namespace BarrocIntens.Sales
         public CustomerCreatePage()
         {
             this.InitializeComponent();
+        }
+
+        // Checks if input was enter if yes sends to create
+        private void NameTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                CreateAccount();
+            }
+        }
+
+        // Checks if input was enter if yes sends to create
+        private void EmailTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                CreateAccount();
+            }
+        }
+
+        // Sends to create if button was pressed
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateAccount();
+        }
+
+        // Creates a new account
+        private void CreateAccount()
+        {
+            // Set error boxes on empty and hidden
+            NameErrorTextBlock.Visibility = Visibility.Collapsed;
+            NameErrorTextBlock.Text = "";
+            EmailErrorTextBlock.Visibility = Visibility.Collapsed;
+            EmailErrorTextBlock.Text = "";
+
+            // Validation
+            bool validationCheck = false;
+            if (string.IsNullOrWhiteSpace(NameTextBox.Text))
+            {
+                NameErrorTextBlock.Text = "Please fill in a name.";
+                validationCheck = true;
+            }
+            if (NameTextBox.Text.Length > 45)
+            {
+                NameErrorTextBlock.Text = "Name is to long.";
+                validationCheck = true;
+            }
+            if (!Helpers.EmailChecker(EmailTextBox.Text))
+            {
+                EmailErrorTextBlock.Text = "Please fill in a valid email";
+                validationCheck = true;
+            }
+            if (EmailTextBox.Text.Length > 255)
+            {
+                EmailErrorTextBlock.Text = "Email is to long";
+                validationCheck = true;
+            }
+            if (validationCheck)
+            {
+                NameErrorTextBlock.Visibility = Visibility.Visible;
+                EmailErrorTextBlock.Visibility = Visibility.Visible;
+
+                return;
+            }
+
+            int lenght = new Random().Next(1, 15);
+            string password = Helpers.PasswordGen(lenght, new Random().Next(0, lenght));
+
+            // Create new user.
+            using (AppDbContext dbContext = new AppDbContext())
+            {
+                dbContext.User.Add(new Models.User
+                {
+                    Name = NameTextBox.Text,
+                    Email = EmailTextBox.Text,
+                    Password = BCrypt.Net.BCrypt.EnhancedHashPassword(password),
+                    RoleId = dbContext.Roles.Where(r => r.Name.ToLower() == "customer").FirstOrDefault().Id
+                });
+                dbContext.SaveChanges();
+            }
+
+            // Send to mail
+            new RegisterEmail(NameTextBox.Text, EmailTextBox.Text, password);
+
+            // Todo add overview page
+            //this.Frame.Navigate(Customers);
         }
     }
 }
---
+++ BarrocIntens/BarrocIntens/Utility/Helpers.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BarrocIntens.Utility
+{
+    internal class Helpers
+    {
+        // Regex validator for emails
+        // This does not check if the email actually exists
+        public static bool EmailChecker(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            try
+            {
+                // Examines the domain part and normalizes it
+                string DomainMapper(Match match)
+                {
+                    IdnMapping idn = new IdnMapping();
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+                    return match.Groups[1].Value + domainName;
+                }
+
+                email = Regex.Replace(email,
+                                      @"(@)(.+)$",
+                                      DomainMapper,
+                                      RegexOptions.None,
+                                      TimeSpan.FromMilliseconds(200));
+
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            try
+            {
+                // Validates and returns true if matches otherwise false.
+                return Regex.IsMatch(email,
+                                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                                    RegexOptions.IgnoreCase,
+                                    TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        // Create a random password
+        public static string PasswordGen(int lenght, int numberOfNonAlphanumericCharacters)
+        {
+            // Character list
+            char[] punctuations = "!@#$%^&*()_-+=[{]};:>|./?".ToCharArray();
+
+            // Validation for lenght and numberOfNonAlphanumericCharacters
+            if (lenght < 1 || lenght > 128)
+            {
+                throw new ArgumentException(nameof(lenght));
+            }
+
+            if (numberOfNonAlphanumericCharacters > lenght || numberOfNonAlphanumericCharacters < 0)
+            {
+                throw new ArgumentException(nameof(numberOfNonAlphanumericCharacters));
+            }
+
+            //  Create new random values.
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                byte[] byteBuffer = new byte[lenght];
+                rng.GetBytes(byteBuffer);
+
+                int count = 0;
+                char[] characterBuffer = new char[lenght];
+
+                for (int iter = 0; iter < lenght; iter++)
+                {
+                    int i = byteBuffer[iter] % 87;
+
+                    if (i < 10)
+                    {
+                        characterBuffer[iter] = (char)('0' + i);
+                    }
+                    else if (i < 36)
+                    {
+                        characterBuffer[iter] = (char)('A' + i - 10);
+                    }
+                    else if (i < 62)
+                    {
+                        characterBuffer[iter] = (char)('a' + i - 36);
+                    }
+                    else
+                    {
+                        characterBuffer[iter] = punctuations[i - 62];
+                        count++;
+                    }
+                }
+
+                if (count >= numberOfNonAlphanumericCharacters)
+                {
+                    return new string(characterBuffer);
+                }
+
+                int j;
+                Random random = new Random();
+
+                for (j = 0; j < numberOfNonAlphanumericCharacters - count; j++)
+                {
+                    int k;
+                    do
+                    {
+                        k = random.Next(0, lenght);
+                    }
+                    while (!char.IsLetterOrDigit(characterBuffer[k]));
+
+                    characterBuffer[k] = punctuations[random.Next(0, punctuations.Length)];
+                }
+
+                return new string(characterBuffer);
+            }
+        }
+    }
+}
