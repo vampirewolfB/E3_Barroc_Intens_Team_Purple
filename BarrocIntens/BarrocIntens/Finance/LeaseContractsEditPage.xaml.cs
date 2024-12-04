6c9a4f91c2f8ca01bc5734fb93e02e3d84e697c8
Tygo van Dorst <D295661@edu.curio.nl>
2024-12-04 12:41:44 +0100

    Added all the contract data to editpage

--- BarrocIntens/BarrocIntens/Finance/LeaseContractsEditPage.xaml.cs
+++ BarrocIntens/BarrocIntens/Finance/LeaseContractsEditPage.xaml.cs
@@ -1,4 +1,6 @@
 using BarrocIntens.Models;
+using BarrocIntens.Uttility.Database;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -24,11 +26,65 @@ namespace BarrocIntens.Finance
     /// </summary>
     public sealed partial class LeaseContractsEditPage : Page
     {
-        private Contract selectedContract;
+        internal Contract SelectedContract { get; private set; }
+        private List<Company> companies;
+        private Company chosenCompany;
 
         public LeaseContractsEditPage()
         {
             this.InitializeComponent();
+            using (AppDbContext db = new AppDbContext())
+            {
+                companies = db.Companies
+                    .Include(c => c.User)
+                    .ToList();
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.Parameter is Contract contract)
+            {
+                SelectedContract = contract;
+                LeaseContractEdit.DataContext = this;
+            }
+            UpdateRadioButtons();
+        }
+        private void UpdateRadioButtons()
+        {
+            if (SelectedContract != null)
+            {
+                // Check if the contract type is "Periodic"
+                PerodicRadioButton.IsChecked = SelectedContract.Type == Contract.PaymentTypes.Perodic; ;
+
+                // Check if the contract type is "Monthly"
+                MonthlyRadioButton.IsChecked = SelectedContract.Type == Contract.PaymentTypes.Monthly; ;
+            }
+        }
+
+        private void CompanyAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                // Filter companies based on user input
+                List<Company> filteredCompanies = companies
+                    .Where(c => c.Name.Contains(sender.Text, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+
+                // Set the filtered companies as the suggestions
+                sender.ItemsSource = filteredCompanies;
+            }
+        }
+
+        private void CompanyAutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            if (args.SelectedItem is Company company)
+            {
+                chosenCompany = company;
+
+                sender.Text = company.Name;
+            }
         }
     }
 }
